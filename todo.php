ac5b645b6d9d5171a50837d6ea212509eaba1f4a
gitjoseluis <inside@cashuba.com>
2014-04-11 10:36:28 +0200

    Solucionado el problema de los tabs

--- todo.php
+++ todo.php
@@ -50,25 +50,25 @@ $( "#tabs" ).tabs();
     
     <div id="content">
  
-    <div id="tabs">
+    
 			<?php
+				$menu= ' ';
+				$content= ' ';
+				
 				$num="SELECT * FROM prioridades";
 				$envio1=mysqli_query($con,$num);
 				
-					
-					echo'<ul>';
-					while($prioridades=mysqli_fetch_array($envio1)){
-					
-						echo'<li><a href="#tabs-'.$prioridades["id_prioridad"].'">'.$prioridades["nombre_prioridad"].'</a></li>';
+					$menu .= '<ul>';
+					while($prioridades=mysqli_fetch_assoc($envio1)){
+						
+						$menu .= '<li><a href="#tabs-'.$prioridades["id_prioridad"].'">'.$prioridades["nombre_prioridad"].'</a></li>';
+						$content .=' <div id="tabs-'.$prioridades["id_prioridad"].'"><p>'.$prioridades["id_prioridad"].'</p></div>';
 					
 					}
-					echo'</ul>';
 					
-					while($prioridades){
-						echo'<div id="tabs-'.$prioridades["id_prioridad"].'"><p>'.$prioridades["id_prioridad"].'</p>
-						 </div>';
-		
-				}
+					$menu .='</ul>';
+					
+					echo '<div id="tabs">'.$menu,$content.'</div>';
 				
 			?>
 			</div>
